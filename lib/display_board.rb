57082b217495f009fd2a274c8a246ee9284456dc
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-13 15:18:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,7 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   lines = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} " 
   puts "#{lines}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "#{lines}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
 end
